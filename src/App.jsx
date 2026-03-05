7b6670cd8520780b4129e712cb4b70f73724b6c6
edra <edralinramos11@gmail.com>
2026-03-05 14:02:11 +0800

    Hello React

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  return <h1 className="text-9xl text-pink-300 box-border">Hello React</h1>
+  return <h1 className="text-9xl text-pink-300 bg-red-50 min-h-screen">Hello React</h1>
   
 }
 
