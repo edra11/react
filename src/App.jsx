5dfdc36c99274ec83cce47c36c121c055778b7b8
edra <edralinramos11@gmail.com>
2026-03-05 14:10:27 +0800

    Hello React anooo Tarahhhgit add .!

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  return <h1 className="text-9xl text-pink-300 bg-red-50 min-h-screen">Hello React ano Tarahhh!!!</h1>
+  return <h1 className="text-9xl text-pink-300 bg-red-50 min-h-screen">Hello React anooo Tarahhh!!!</h1>
   
 }
 
