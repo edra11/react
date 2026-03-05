f9c655b6199d0718f40faab96bf6976b8c4469ba
edra <edralinramos11@gmail.com>
2026-03-05 15:45:13 +0800

    components

---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class main extends Component {
+  render() {
+    return (
+      <div className="flex-1">main</div>
+    )
+  }
+}
+
+export default main
